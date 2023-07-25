462b111a2337e62d3c7847ae297bd6a5c401a4fb
luissavighago <55593826+luissavighago@users.noreply.github.com>
2023-07-24 22:35:49 -0300

    #animações ao scrollar a pagina

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//menu responsivo
 const mobileMenu = () => {
   let menu = document.querySelector(".nav-mobile-menu")
   if(menu.classList.contains("open")){
@@ -7,9 +8,21 @@ const mobileMenu = () => {
   }
 }
 
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
+//animações onScroll
+window.sr = ScrollReveal({reset:true});
+sr.reveal('#home-info-itens-1',{origin: 'top',distance: '50px',duration: 1000,delay:300});
+sr.reveal('#home-info-itens-2',{origin: 'top',distance: '50px',duration: 1000,delay:400});
+sr.reveal('#home-info-itens-3',{origin: 'top',distance: '50px',duration: 1000,delay:600});
+sr.reveal('#home-info-itens-4',{origin: 'top',distance: '50px',duration: 1000,delay:700});
+
+sr.reveal('#about',{duration: 1000});
+
+sr.reveal('#skills',{duration: 1000});
+sr.reveal('.skill-img',{origin: 'bottom',distance: '100px',rotate: {x:80, y:80,z:0},duration:1000,delay:500});
+
+sr.reveal('#projects',{duration: 1000});
+sr.reveal('.project-animation-right',{origin: 'right',distance: '100px',duration: 1000,delay:500});
+sr.reveal('.project-animation-left',{origin: 'left',distance: '100px',duration: 1000,delay:500});
+
+sr.reveal('#contact',{duration: 1000});
+sr.reveal('.form-contact',{origin: 'right',distance: '100px',duration: 1000,delay:500});
